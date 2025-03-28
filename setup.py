7594b1633f19089c0c807d0937f141b2e9558df5
Takashi Miyoshi <miyoshi@alleged.local>
2025-03-28 10:42:52 +0900

    updated.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name="android-ctrl",
     version="0.1.0",
     description="A CLI tool to manage Android screens and apps",
-    author="Your Name",
-    author_email="your.email@example.com",
-    license="MIT",
     packages=find_packages(),
     install_requires=[
         "fire"
@@ -18,10 +15,5 @@ setup(
             "swipe-screens=app.swipe_screens:main",
         ],
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ], 
     python_requires=">=3.6",
 )
