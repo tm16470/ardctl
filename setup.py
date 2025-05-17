f153a0fb106e4b483164814b85497ce02ec43949
tm16470 <takashi.miyoshi@gmail.com>
2025-05-17 22:27:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     entry_points={
         "console_scripts": [
             "remove-default-apps=app.remove_default_apps:main",
-            "ard-open=app.ard-screens:main",
+            "ard-open=app.ard-open:main",
             "ard-align=app.ard-align:main",
             "ard-swipe=app.ard-swipe:main",
         ],
