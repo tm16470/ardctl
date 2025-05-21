bbbbe6404035a08ccc84088a3bc40dfa54205950
tm16470 <takashi.miyoshi@gmail.com>
2025-05-21 22:16:46 +0900

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -4,9 +4,6 @@ class ArdCtl:
     def open(self):
         subprocess.run(["scrcpy"])
 
-    def close(self):
-        print("未実装: close コマンド")
-
 if __name__ == "__main__":
     import fire
-    fire.Fire(ArdCtl)
+    fire.Fire(ArdCtl())
